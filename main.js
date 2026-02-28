c1fc8a322954ad0384ee94a7606217c6a8cbc300
Tom-De-Jong <tomdejong2009@gmail.com>
2026-02-28 22:55:08 +0100

    added a working text editor and app icons at the bottom. also made extra css animations

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function login(){
 document.querySelector(".loginCard").className = "loginCard swipeDown";
 setTimeout(() => {
 document.querySelector(".loginScreen").style.display = "none"
+document.querySelector(".taskBar").className = "taskBar slideUp"
+document.querySelector(".navBar").className = "navBar slideDown"
 document.querySelector(".desktop").style.display = "block";
 }, 1000);
 
